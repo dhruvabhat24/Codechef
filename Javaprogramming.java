bfa408bd55f41b9bf687be32ba0d52ad05cc9115
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-12 11:17:50 +0530

    Delete Javaprogramming.java

--- Javaprogramming.java
+++
@@ -1,8 +0,0 @@
-//multiplication and addition of numbers in java 
-class CodeChef{
-  public static void main(String[] args) {
-    // replace the _ with correct value
-    System.out.println(8 * 9);
-    System.out.print((100 + 200) * 2);
-  } 
-}
