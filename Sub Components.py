57d36c6fb7c479fff93d8141065c9cdf07aac3aa
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-14 15:40:15 +0530

    Subcomponents 1

---
+++ Sub Components.py
@@ -0,0 +1,19 @@
+# Solution as follows
+
+A = 15
+C = 17
+if A%2 == 0 and C%2 == 0:
+    print('Both A and C are even')
+elif A%2 != 0 and C%2 != 0:
+    print('Both A and C are odd')
+else:
+    print('A is odd and C is even or vice versa')
+    
+A = 15
+C = 18
+if A%2 == 0 and C%2 == 0:
+    print('Both A and C are even')
+elif A%2 != 0 and C%2 != 0:
+    print('Both A and C are odd')
+else:
+    print('A is odd and C is even or vice versa')
