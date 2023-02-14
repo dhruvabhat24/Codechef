c2c03dcd5ff61fcff56f69be0458e24cb4c5fa32
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-14 15:38:15 +0530

    Problems Generate example

---
+++ Make avg.py
@@ -0,0 +1,9 @@
+# Solution as follows
+
+print("For A = 3 and C = 4, the output is", -1)
+
+print("For A = 3 and C = 5, the output is", 4)
+
+print("For A = 2 and C = 4, the output is", 3)
+
+print("For A = 2 and C = 5, the output is", -1)
